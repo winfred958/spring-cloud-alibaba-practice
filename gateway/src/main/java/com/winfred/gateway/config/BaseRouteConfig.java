f49333f82c6c8551f25c51b91171c9126db93275
kevin_678 <kevin_678@126.com>
2020-02-10 16:34:34 +0800

    feat: update

---
+++ gateway/src/main/java/com/winfred/gateway/config/BaseRouteConfig.java
@@ -0,0 +1,16 @@
+package com.winfred.gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BaseRouteConfig {
+
+//    @Bean
+//    public RouteLocator test(RouteLocatorBuilder routeLocatorBuilder) {
+//
+//        return routeLocatorBuilder.routes().route("test", r -> {
+//            r.path("local").uri("www.baidu.com");
+//        })
+//                .build();
+//    }
+}
---
+++ order/order-api/src/main/java/com/winfred/order/api/OrderService.java
@@ -0,0 +1,4 @@
+package com.winfred.order.api;
+
+public interface OrderService {
+}
