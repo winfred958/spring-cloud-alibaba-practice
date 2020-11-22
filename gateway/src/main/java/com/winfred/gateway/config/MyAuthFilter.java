f071df1e821a946a94a6fc6117108a4cf4404d5e
kevin_789 <kevin_789@126.com>
2020-11-23 00:31:06 +0800

    feat: update

--- gateway/src/main/java/com/winfred/gateway/config/MyAuthFilter.java
+++ gateway/src/main/java/com/winfred/gateway/config/MyAuthFilter.java
@@ -1,43 +1,35 @@
 package com.winfred.gateway.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpCookie;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import java.net.URI;
-
-
+@ConditionalOnProperty(prefix = "switch", name = "token.check", havingValue = "true")
 @Configuration
 @Slf4j
-public class UserDefineGatewayFilter {
+public class MyAuthFilter {
 
-  /**
-   * echo took
-   *
-   * @return
-   */
   @Bean
-  @Order(Ordered.HIGHEST_PRECEDENCE)
-  public GlobalFilter echoTookFilter() {
+  @Order(Ordered.HIGHEST_PRECEDENCE + 1)
+  public GlobalFilter cookiesFilter() {
     return new GlobalFilter() {
       @Override
       public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        URI uri = exchange.getRequest().getURI();
-        long start = System.currentTimeMillis();
+        MultiValueMap<String, HttpCookie> cookies = exchange.getRequest().getCookies();
+        log.info("cookies.size = {}", cookies.size());
+
         return chain
-                .filter(exchange)
-                .then(Mono.fromRunnable(() -> {
-                  long end = System.currentTimeMillis();
-                  log.info("request took {} => rui  {}", end - start, uri);
-                }));
+                .filter(exchange);
       }
     };
   }
-
 }
