5d62242823b2c83522f2d2ca010a81dc7799f9c9
kevin_789 <kevin_789@126.com>
2020-11-22 22:07:38 +0800

    feat: update gateway + filter

---
+++ gateway/src/main/java/com/winfred/gateway/config/UserDefineGatewayFilter.java
@@ -0,0 +1,34 @@
+package com.winfred.gateway.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+
+@Configuration
+@Slf4j
+public class UserDefineGatewayFilter {
+
+  @Bean
+  public GlobalFilter echoTookFilter() {
+    return new GlobalFilter() {
+      @Override
+      public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        URI uri = exchange.getRequest().getURI();
+        long start = System.currentTimeMillis();
+        return chain
+                .filter(exchange)
+                .then(Mono.fromRunnable(() -> {
+                  long end = System.currentTimeMillis();
+                  log.info("request took {} => rui  {}", end - start, uri);
+                }));
+      }
+    };
+  }
+}
--- item/item-rest/src/main/java/com/winfred/item/rest/ItemInfoRest.java
+++ item/item-rest/src/main/java/com/winfred/item/rest/ItemInfoRest.java
@@ -4,6 +4,7 @@ import com.winfred.core.entity.base.BaseResponse;
 import com.winfred.item.api.ItemInfoApi;
 import com.winfred.item.entity.ItemInfo;
 import com.winfred.item.service.ItemService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Slf4j
 public class ItemInfoRest implements ItemInfoApi {
 
   @Autowired
