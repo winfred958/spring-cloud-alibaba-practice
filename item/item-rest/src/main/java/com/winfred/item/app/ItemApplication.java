0926430a45eb57d154da79eecc2394525faeb2ab
kevin_789 <kevin_789@126.com>
2020-11-22 21:42:58 +0800

    feat: update

--- item/item-rest/src/main/java/com/winfred/item/app/ItemApplication.java
+++ item/item-rest/src/main/java/com/winfred/item/app/ItemApplication.java
@@ -1,5 +1,6 @@
 package com.winfred.item.app;
 
+import com.alibaba.nacos.api.config.annotation.NacosConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
